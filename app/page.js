4cd75a0107ca1c3d6e4c55df2910075965a22ed8
vaibhav singh <vaibhav05dec@gmail.com>
2026-09-10 22:21:16 +0530

    feat: implement homepage layout with GSAP scroll-triggered animations and navigation menu

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@
 
 import { useState, useEffect, useRef } from "react";
 import CustomCursor from "./components/CustomCursor";
-import Hero3D from "./components/Hero3D";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
